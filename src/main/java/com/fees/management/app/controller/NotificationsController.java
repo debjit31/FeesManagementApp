2a66b091bd01f6295e692028f2b337f04c757a4e
debjit31 <debjitchatterjee31@gmail.com>
2023-09-09 21:54:54 +0530

    Bypassing CORS on all endpoints

--- src/main/java/com/fees/management/app/controller/NotificationsController.java
+++ src/main/java/com/fees/management/app/controller/NotificationsController.java
@@ -2,10 +2,12 @@ package com.fees.management.app.controller;
 
 import com.fees.management.app.service.ScheduledNotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/notifications")
 public class NotificationsController {
